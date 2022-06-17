572857e99a3365a99e43262553c3d4df612650f5
Pranalijadhav03 <jadhav.pranali2403@gmail.com>
2022-06-17 20:26:15 +0530

    Added: CreateTable

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,16 @@
 ﻿-----------------Creating the DataBase AddressBook (UC1)-----------------
 Create database AdvAddressBookDB1;
 use AdvAddressBookDB1;
+
+-----------------Creating the AddressBook Table (UC2)--------------------
+Create Table AddressBook1(
+	FirstName varchar(50) not null,
+	LastName varchar(50),
+	Address varchar(255),
+	City varchar(50),
+	StateName varchar(50),
+	ZipCode int,
+	PhoneNum Varchar(10),
+	EmailId varchar(50)
+);
+
