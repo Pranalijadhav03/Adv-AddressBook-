fd74ef9bcf573cfe4e3505a2ca1455ecfb771691
Pranalijadhav03 <jadhav.pranali2403@gmail.com>
2022-06-17 20:33:49 +0530

    Added: InsertNewContact

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -14,3 +14,11 @@ Create Table AddressBook1(
 	EmailId varchar(50)
 );
 
+------------------Inserting Value Into AddressBook Table (UC3)--------------
+Insert Into AddressBook1 Values('Pranali', 'Prakash', 'Jadhav', 'Mumbai', 'Maharashtra', 400099, '7004670887', 'pj321626@gmail.com'),
+						('Dipti', 'Sunil', 'Satam', 'Pune', 'Maharashtra', 111045, '7543065510', 'Dipti123@gmail.com'),	
+						('Nutan', 'Pravin', 'Katam', 'Bengluru', 'Maharashtra', 5600301, '7004678878', 'Nuts1029@gmail.com'),	
+						('Rakesh', 'Kumar', 'Sharma', 'Delhi' ,'Maharashtra', 356809, '9852364170', 'RJ852@gmail.com'),	
+						('Rachit', 'Yeshwant', 'Kanavale', 'Andheri', 'Maharashtra', 822102, '9852741630', 'Rachit741@gmail.com');
+
+select * from AddressBook1
